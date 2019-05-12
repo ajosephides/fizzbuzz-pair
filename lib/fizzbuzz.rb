f20f015103526831c4d3d1fc44023466efccb41d
Alexis Josephides <ajosephides@gmail.com>
2019-05-12 10:29:40 +0100

    added test cases and passes

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,9 @@
+def fizzbuzz(input)
+
+  if input == 3
+    return 'fizz'
+  else 
+    return input.to_s
+  end
+  
+end
---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,11 @@
+require 'fizzbuzz.rb'
+
+describe 'fizzbuzz' do
+  it 'returns "fizz" when passed 3' do
+    expect(fizzbuzz(3)).to eq 'fizz'
+  end
+
+  it 'returns "2" when passed 2' do
+    expect(fizzbuzz(2)).to eq '2'
+  end
+end 
