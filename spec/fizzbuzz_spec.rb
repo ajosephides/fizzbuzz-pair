67ed5bcd7a0abd8edbc58b3fc18f819828416ef4
Alexis Josephides <ajosephides@gmail.com>
2019-05-12 10:35:32 +0100

    added test for buzz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,4 +8,8 @@ describe 'fizzbuzz' do
   it 'returns "2" when passed 2' do
     expect(fizzbuzz(2)).to eq '2'
   end
+
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz' 
+  end
 end 
